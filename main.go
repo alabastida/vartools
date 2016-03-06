8f374ae7057dfd1a9a751756e7f778b0be81b6b2
xulu <xulu@meilishuo.com>
2016-03-07 07:58:03 +0800

    add

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    _ "fmt"
+	_ "time"
+	_ "strconv"  
+    "github.com/tealeg/xlsx"
+	_ "database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	_ "log"
+)
+
+const SIZE = 1000000
+
+func main() {
+
+	a := 1
+	b := "test"
+	
+    var file *xlsx.File
+    var sheet *xlsx.Sheet
+    var err error
+	
+	use(a, file, sheet, err)
+	xulu.use()
+	un.use()
+	
+	not.use()
+	xulu.use(
+	avoid.use()
+}
+
+func use(vals ...interface{}) {
+	for _, val := range vals {
+		_ = val
+	}
+}
